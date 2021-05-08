4cd978fadf827b7c5add39e9ce5054bacb149858
Joy Ye <zijunye001@gmail.com>
2021-05-07 20:30:12 -0400

    happy

--- js/script.js
+++ js/script.js
@@ -64,11 +64,14 @@ window.onload=function(){
 		page1.style.display="none";
 		page2.style.display="block";
 		page3.style.display="block";
+		page4.style.display="block";
 		page3.style.top="100%"
 
 		setTimeout(function(){
 			page2.setAttribute("class","page fadeOut");
 			page3.setAttribute("class","page fadeIn");
+			page3.setAttribute("class","page fadeOut");
+			page4.setAttribute("class","page fadeIn");
 		},5500)
 
 		
