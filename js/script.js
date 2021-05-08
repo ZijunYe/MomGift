736fa8dd9986065f51dca8498aa2c646c08e97d0
Jun <57678262+ZijunYe@users.noreply.github.com>
2021-05-07 21:50:41 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,62 +15,76 @@ window.onload=function(){
 	console.log("屏幕可用工作区高度：" + window.screen.availHeight);
 	console.log("屏幕可用工作区宽度：" + window.screen.availWidth);
 
+	
 	var music=document.getElementById("music");
-	var audio=document.getElementsByTagName("audio");
-	var n11=document.getElementById("n1");
-	var n12=document.getElementById("n2");
-	audio.volume = 0.6;
+	var audio=document.getElementsByTagName("audio")[0];
 
-	n11.addEventListener("touchstart",function(event){
-		if(audio.volume>=0&&audio.volume<=1){
-			audio.volume=audio.volume+0.1;
-		}	
-	},false);
-	n12.addEventListener("touchstart",function(event){
-		if(audio.volume>=0&&audio.volume<=1){
-			audio.volume=audio.volume-0.1;
-		}
-	},false);
+var n11=document.getElementById("n1");//加音量
 
-	audio.addEventListener("ended",function(event){
-		music.setAttribute("class","");
-		//music.style.animationPlayState="paused";
-		//music.style.webkianimationPlayState="paused";
-	},false)
+var n12=document.getElementById("n2");//减音量
 
-	music.onclick=function(){//click有0.3秒的延迟
-		if(audio.paused){
-			audio.play();
-			this.setAttribute("class","play");
-			//music.style.animationPlayState="running";//兼容性比较差
-			music.style.webkitanimationPlayState="running";//苹果兼容
-		}else{
-			audio.pause();
-			this.setAttribute("class","");
-			//this.style.animationPlayState="paused";
-		      this.style.webkitanimationPlayState="paused";
-		}
-	}
-	music.addEventListener("touchstart",function(event){
-		if(audio.paused){
-			audio.play();
-			this.setAttribute("class","play");
-			}else{
-			audio.pause();
-			this.setAttribute("class","");
-			}
-	},false);
-	page1.addEventListener("touchstart",function(event){
-		page1.style.display="none";
-		page2.style.display="block";
-		page3.style.display="block";
-		page3.style.top="100%"
+audio.volume = 0.6;
 
-		setTimeout(function(){
-			page2.setAttribute("class","page fadeOut");
-			page3.setAttribute("class","page fadeIn");
-		},5500)
+n11.addEventListener("touchstart",function(event){
 
+	if(audio.volume>=0&&audio.volume<=1){
+	
+		audio.volume=audio.volume+0.1;
 		
-	},false);
-}
+	}	
+	
+},false);
+
+n12.addEventListener("touchstart",function(event){
+
+	if(audio.volume>=0&&audio.volume<=1){
+	
+		audio.volume=audio.volume-0.1;
+		
+	}
+	
+},false);
+
+audio.addEventListener("ended",function(event){
+
+	music.setAttribute("class","");
+	
+},false)
+
+music.addEventListener("touchstart",function(event){
+
+	if(audio.paused){
+	
+		audio.play();
+		
+		this.setAttribute("class","play");
+		
+		}else{
+		
+		audio.pause();
+		
+		this.setAttribute("class","");
+		
+		}
+		
+},false);
+
+page1.addEventListener("touchstart",function(event){
+
+	page1.style.display="none";
+	
+	page2.style.display="block";
+	
+	page3.style.display="block";
+	
+	page3.style.top="100%"
+
+	setTimeout(function(){
+	
+		page2.setAttribute("class","page fadeOut");
+		
+		page3.setAttribute("class","page fadeIn");
+		
+	},5500)
+	
+},false);
