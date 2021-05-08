e959a47b4dbb22915a7d4edfa720af7c51f2864e
Joy Ye <zijunye001@gmail.com>
2021-05-08 15:07:10 -0400

    adjust

--- js/script.js
+++ js/script.js
@@ -1,44 +1,76 @@
-(function () {
+window.onload=function(){
+    
+    console.log("网页可见区域宽：" + document.body.clientWidth);
+    console.log("网页可见区域高：" + document.body.clientHeight);
+    console.log("网页可见区域宽（包括边线的宽）：" + document.body.offsetWidth );
+    console.log("网页可见区域高（包括边线的宽）：" + document.body.offsetHeight);
+    console.log("网页正文全文宽：" + document.body.scrollWidth);
+    console.log("网页正文全文高：" + document.body.scrollHeight);
+    console.log("网页被卷去的高：" + document.body.scrollTop);
+    console.log("网页被卷去的左：" + document.body.scrollLeft);
+    console.log("网页正文部分上：" + window.screenTop);
+    console.log("网页正文部分左：" + window.screenLeft);
+    console.log("屏幕分辨率的高：" + window.screen.height);
+    console.log("屏幕分辨率的宽：" + window.screen.width);
+    console.log("屏幕可用工作区高度：" + window.screen.availHeight);
+    console.log("屏幕可用工作区宽度：" + window.screen.availWidth);
 
-    var player = document.getElementById('player');
-    var p1 = document.getElementById('p1');
+    var music=document.getElementById("music");
+    var audio=document.getElementsByTagName("audio")[0];
+    var n11=document.getElementById("n1");
+    var n12=document.getElementById("n2");
+    audio.volume = 0.6;
 
-    // 判断是移动端还是桌面端
-    if ('ontouchstart' in document.documentElement) {
-        player.addEventListener('touchstart', touchPlayer, false);  // 监听播放器的触摸事件
-        p1.addEventListener('touchstart', touchPage1, false);       // 监听第一页的触摸事件
-    } else {
-        // 桌面端采用点击代替触摸
-        p1.addEventListener('click', touchPage1, false);
-        player.addEventListener('click', touchPlayer, false);
-    }
-
-
-    function touchPlayer() {
-        var music = document.getElementById('music');
-        var player_disc = document.getElementById('player_disc');
-        
-        if (music.paused) {
-            music.play();
-            player_disc.classList.remove('paused');
-        } else {
-            music.pause();
-            player_disc.classList.add('paused');
+    n11.addEventListener("touchstart",function(event){
+        if(audio.volume>=0&&audio.volume<=1){
+            audio.volume=audio.volume+0.1;
+        }   
+    },false);
+    n12.addEventListener("touchstart",function(event){
+        if(audio.volume>=0&&audio.volume<=1){
+            audio.volume=audio.volume-0.1;
         }
-    }
+    },false);
 
+    audio.addEventListener("ended",function(event){
+        music.setAttribute("class","");
+        //music.style.animationPlayState="paused";
+        //music.style.webkianimationPlayState="paused";
+    },false)
 
-    function touchPage1() {
-        var p2 = document.getElementById('p2');
-        var p3 = document.getElementById('p3');
+    //music.onclick=function(){//click有0.3秒的延迟
+    //  if(audio.paused){
+    //      audio.play();
+    //      this.setAttribute("class","play");
+    //      //music.style.animationPlayState="running";//兼容性比较差
+    //      //music.style.webkitanimationPlayState="running";//苹果兼容
+    //  }else{
+    //      audio.pause();
+    //      this.setAttribute("class","");
+    //      //this.style.animationPlayState="paused";
+    //      //this.style.webkitanimationPlayState="paused";
+    //  }
+    //}
+    music.addEventListener("touchstart",function(event){
+        if(audio.paused){
+            audio.play();
+            this.setAttribute("class","play");
+            }else{
+            audio.pause();
+            this.setAttribute("class","");
+            }
+    },false);
+    page1.addEventListener("touchstart",function(event){
+        page1.style.display="none";
+        page2.style.display="block";
+        page3.style.display="block";
+        page3.style.top="100%"
 
-        p1.classList.add('hide');
-        p2.classList.remove('hide');
-        p3.classList.remove('hide');
+        setTimeout(function(){
+            page2.setAttribute("class","page fadeOut");
+            page3.setAttribute("class","page fadeIn");
+        },5500)
 
-        setTimeout(function () {
-            p2.classList.add('fadeout');
-            p3.classList.add('fadein');
-        }, 4000);
-    }
-}());
+        
+    },false);
+}
