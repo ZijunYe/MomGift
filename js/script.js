1da2163a8181d7c65164ae9b3f6102fc6f3e0e4a
Jun <57678262+ZijunYe@users.noreply.github.com>
2021-05-07 22:00:04 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,90 +1,107 @@
-window.onload=function(){
-	
-	console.log("网页可见区域宽：" + document.body.clientWidth);
-	console.log("网页可见区域高：" + document.body.clientHeight);
-	console.log("网页可见区域宽（包括边线的宽）：" + document.body.offsetWidth );
-	console.log("网页可见区域高（包括边线的宽）：" + document.body.offsetHeight);
-	console.log("网页正文全文宽：" + document.body.scrollWidth);
-	console.log("网页正文全文高：" + document.body.scrollHeight);
-	console.log("网页被卷去的高：" + document.body.scrollTop);
-	console.log("网页被卷去的左：" + document.body.scrollLeft);
-	console.log("网页正文部分上：" + window.screenTop);
-	console.log("网页正文部分左：" + window.screenLeft);
-	console.log("屏幕分辨率的高：" + window.screen.height);
-	console.log("屏幕分辨率的宽：" + window.screen.width);
-	console.log("屏幕可用工作区高度：" + window.screen.availHeight);
-	console.log("屏幕可用工作区宽度：" + window.screen.availWidth);
+window.onload = function() {
+    // 获取元素
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+    var lantern = document.getElementById("p1-lantern");
 
-	
-	var music=document.getElementById("music");
-	var audio=document.getElementsByTagName("audio")[0];
+    var music = document.getElementById("music");
+    var audio = document.getElementsByTagName("audio")[0];
 
-var n11=document.getElementById("n1");//加音量
+    // 音乐播放完毕时，动画停止
+    audio.addEventListener("ended", function(event) {
+        music.setAttribute("class", "");
+        // this.style.animationPlayState = "paused";
+        // this.style.webkitAnimationPlayState = "paused";
+    }, false)
 
-var n12=document.getElementById("n2");//减音量
+    // 点击音乐图标，控制音乐播放效果
+    music.addEventListener("touchstart", function() {
+        if (audio.paused) {
+            audio.play();
+            music.setAttribute("class", "play");
+        } else {
+            audio.pause();
+            music.setAttribute("class", "");
+        }
+    }, false);
 
-audio.volume = 0.6;
+    // 点击屏幕，开启好运2016
+    lantern.addEventListener("click", function() {
+        page1.style.display = "none";
+        page2.setAttribute("class", "page");
+        page2.style.display = "block";
 
-n11.addEventListener("touchstart",function(event){
+        setTimeout(function() {
+            page3.style.display = "block";
+            page3.style.top = "100%";
+            page2.setAttribute("class", "page fadeOut");
+            page3.setAttribute("class", "page fadeIn");
+        }, 5500)
+    }, false)
 
-	if(audio.volume>=0&&audio.volume<=1){
-	
-		audio.volume=audio.volume+0.1;
-		
-	}	
-	
-},false);
+    //手指滑动切换页面--第一页
+    page1.addEventListener("touchstart", function(e) {
+        start_y = e.touches[0].clientY;
+    }, false)
+    page1.addEventListener("touchend", function(e) {
+        end_y = e.changedTouches[0].clientY;
+        var delta_y = end_y - start_y;
+        if (Math.abs(delta_y) < 0.2 * window.innerHeight) {
+            return;
+        } else {
+            if (delta_y < 0) { //下一页
+                page1.style.display = "none";
+                page2.setAttribute("class", "page");
+                page2.style.display = "block";
 
-n12.addEventListener("touchstart",function(event){
+            } else { //上一页
+                page1.style.display = "none";
 
-	if(audio.volume>=0&&audio.volume<=1){
-	
-		audio.volume=audio.volume-0.1;
-		
-	}
-	
-},false);
+                page3.style.display = "block";
+                // blessing.style.opacity = 1;
+            }
+        }
+    }, false)
 
-audio.addEventListener("ended",function(event){
+    //手指滑动切换页面--第二页
+    page2.addEventListener("touchstart", function(e) {
+        start_y = e.touches[0].clientY;
+    }, false)
+    page2.addEventListener("touchend", function(e) {
+        end_y = e.changedTouches[0].clientY;
+        var delta_y = end_y - start_y;
+        if (Math.abs(delta_y) < 0.2 * window.innerHeight) {
+            return;
+        } else {
+            if (delta_y < 0) { //下一页
+                page2.style.display = "none";
+                page3.style.display = "block";
+            } else { //上一页
+                page1.style.display = "block";
+                page2.style.display = "none";
+            }
+        }
+    }, false)
 
-	music.setAttribute("class","");
-	
-},false)
-
-music.addEventListener("touchstart",function(event){
-
-	if(audio.paused){
-	
-		audio.play();
-		
-		this.setAttribute("class","play");
-		
-		}else{
-		
-		audio.pause();
-		
-		this.setAttribute("class","");
-		
-		}
-		
-},false);
-
-page1.addEventListener("touchstart",function(event){
-
-	page1.style.display="none";
-	
-	page2.style.display="block";
-	
-	page3.style.display="block";
-	
-	page3.style.top="100%"
-
-	setTimeout(function(){
-	
-		page2.setAttribute("class","page fadeOut");
-		
-		page3.setAttribute("class","page fadeIn");
-		
-	},5500)
-	
-},false);
+    //手指滑动切换页面--第三页
+    page3.addEventListener("touchstart", function(e) {
+        start_y = e.touches[0].clientY;
+    }, false)
+    page3.addEventListener("touchend", function(e) {
+        end_y = e.changedTouches[0].clientY;
+        var delta_y = end_y - start_y;
+        if (Math.abs(delta_y) < 0.2 * window.innerHeight) {
+            return;
+        } else {
+            if (delta_y < 0) { //下一页
+                page1.style.display = "block";
+                page3.style.display = "none";
+            } else { //上一页
+                page2.setAttribute("class", "page");
+                page2.style.display = "block";
+                page3.style.display = "none";
+            }
+        }
+    }, false)
+}
